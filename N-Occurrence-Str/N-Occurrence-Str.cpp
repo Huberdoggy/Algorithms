f4ac4a5dad91b480b76b113238d41d3f8ecfebec
Huberdoggy <huberdoggy@gmail.com>
2023-05-20 09:12:33 -0500

    altered program to use a global map for tracking which letters in the generated string, if any, have repeated occurrences. Map is emptied each call to determineOccurrences. String is now initialized within do/while, instead of being passed by ref and reassinged null each loop through main

--- N-Occurrence-Str/N-Occurrence-Str.cpp
+++ N-Occurrence-Str/N-Occurrence-Str.cpp
@@ -1,40 +1,50 @@
 /*
+Program which allows the user to input a number from 2 to 30 and generates
+a random string of N length. If string has characters of frequency > 1, will
+display the count of said repeated character/s.
 
+TODO - Find a way to ensure every character in the generated string has normalized
+frequency
 */
 
 #include "common_includes.h"
-
+#include <unordered_map>
 
 inline int genRandChar();
-std::pair<std::string, int> createStr(std::string&, int);
+std::string createStr(int);
 void checkInput(int&);
-int determineOccurences(const std::string&);
+void determineOccurences(const std::string&);
 
 constexpr int L_ALPHA_START{ 97 }, RANGE_END{ 26 }, MAX_ALLOWED{ 30 },
 MIN_NEEDED{ 2 };
 
+std::unordered_map<char, int> M;
 
 int main() {
 
     srand(static_cast<unsigned>(time(0)));
     char goAgain;
     int num;
-    std::string randStr;
-    std::pair<std::string, int> p;
+
 
     do
     {
         std::cout << "Enter an integer and I will generate a random string\n"
             << "of 'N' letters => ";
         checkInput(num);
-        p = createStr(randStr, num);
+        std::string randStr = createStr(num);
 
-        std::cout << "Random string is: " << p.first << std::endl;
-        if (p.second)
-            std::cout << "REPEATING CHARACTERS: " << p.second << std::endl;
-        randStr = "";
-        std::cout << "Generate a new string? ['Y' 'N']? "; std::cin >> goAgain;
-        std::cout << "\n\n";
+        std::cout << "\nRandom string is: " << randStr << std::endl;
+        if (!(M.empty())) {
+            std::cout << "REPEATED OCCURRENCES OF LETTERS:\n";
+            for (auto& k : M)
+            {
+                std::cout << "'" << k.first << "'" << " -> " << k.second
+                    << std::endl;
+            }
+        }
+        std::cout << "\nGenerate a new string? ['Y' 'N']? "; std::cin >> goAgain;
+        system("cls"); // Clear console
     } while (tolower(goAgain) == 'y');
 
     return 0;
@@ -55,38 +65,38 @@ void checkInput(int& num) {
 
 inline int genRandChar() {
 
-    return L_ALPHA_START + (rand() % RANGE_END);
+    return L_ALPHA_START + (rand() % RANGE_END); // Offset 97 ('a'), range end
+    // 1 + 'z'
 }
 
 
-std::pair<std::string, int> createStr(std::string& str, int N) {
+std::string createStr(int N) {
 
-    std::pair<std::string, int> p;
+    std::string str;
 
     for (int i = 0; i < N; i++)
     {
-        char letter = static_cast<char>(genRandChar());
+        char letter = static_cast<char>(genRandChar()); // Covert returned int
         str += letter;
     }
 
-    p.first = str;
-    p.second = determineOccurences(str);
-    return p;
+    std::sort(str.begin(), str.end());
+    determineOccurences(str);
+    return str;
 }
 
 
-int determineOccurences(const std::string& str) {
+void determineOccurences(const std::string& str) {
 
-    int occurences{ 0 };
+    M.clear(); // global, so need to reset each time the user re-runs prog
 
-    for (int i = 0; i < (str.size() - 1); i++)
+    for (int i = 0; i < str.length(); i++)
     {
 
         if (std::count(str.begin(), str.end(), str[i]) > 1) {
 
-            occurences++;
+            M[str[i]]++; // Add count to map index of repeated letter
         }
     }
 
-    return (occurences > 0 ? occurences : 0);
 }
