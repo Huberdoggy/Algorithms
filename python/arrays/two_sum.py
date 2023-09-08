d875a1face8d95b950b0baab4f027035aadc86c6
Huberdoggy <huberdoggy@gmail.com>
2023-09-08 07:39:52 -0500

    corrected one-off in nested range. Added todo dir of programs to ignore

--- python/arrays/two_sum.py
+++ python/arrays/two_sum.py
@@ -25,7 +25,7 @@ Output: [0,1]
 
 class Solution(object):
     def twoSum(self, nums, target):
-        for i in range(len(nums) - 1):
+        for i in range(len(nums)):
             for j in range(len(nums) - 1, i, -1):
                 if nums[i] + nums[j] == target:
                     return [i, j]
