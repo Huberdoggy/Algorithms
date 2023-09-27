58d2541fa578f6d5ac4a2d6ffcb7b55f79f300de
Huberdoggy <huberdoggy@gmail.com>
2023-09-27 09:01:31 -0500

    made a class version of palindromes challenge, taking user string input

---
+++ python/strings/palindromes.py
@@ -0,0 +1,34 @@
+import re
+
+
+class Word:
+    def __init__(self) -> None:
+        self._user_word = input("Enter a word => ")
+        # Anything shorter wouldn't really make sense to eval
+        self._regex = re.compile(r"[a-zA-Z]{3,}")
+        self._is_pal = self._checkPalindrome(self._user_word, self._regex, self._Iterate)
+
+    def _Iterate(self, str_word: str):
+        start, end = 0, len(str_word) - 1
+        while start < end:
+            if str_word[start].lower() != str_word[end].lower():
+                return False
+            start += 1
+            end -= 1
+        return True
+
+    def _checkPalindrome(self, str_word: str, reg, my_func):
+        if not str_word:
+            return False
+        elif not re.fullmatch(reg, str_word):
+            return False
+        else:
+            return my_func(str_word)  # Pass the bool back caller, stored in class attrib
+
+
+x = Word()
+print(
+    f"The word you entered: '{x._user_word}' is identical in reverse"
+    if x._is_pal
+    else f"'{x._user_word}' is NOT a palindrome"
+)
